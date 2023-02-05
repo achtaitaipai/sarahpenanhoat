e969bb524bfe27f3d889a5865fc07752cd988ff9
achtaitaipai <charles.cailleteau@gmail.com>
2023-02-05 11:42:57 +0100

    intersection observer component

--- src/Observable-wrapper.ts
+++ src/Observable-wrapper.ts
@@ -1,52 +1,32 @@
-let options = {
-    rootMargin: '0px',
-    threshold: 0.4
+export class ObserbleWrapper extends HTMLAnchorElement {
+  static _observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entrie) => {
+        if (entrie.intersectionRatio > 0)
+          entrie.target?.classList.remove("not-visible");
+      });
+    },
+    {
+      rootMargin: "0px",
+      threshold: 0.5,
+    }
+  );
+
+  private _observables?: NodeListOf<HTMLElement>;
+
+  connectedCallback() {
+    if ("IntersectionObserver" in window) {
+      this._observables = this.querySelectorAll<HTMLElement>("[anim-onscroll]");
+      this._observables.forEach((observable) => {
+        ObserbleWrapper._observer.observe(observable);
+        observable.classList.add("not-visible");
+      });
+    }
   }
-  
-//   let observer = new IntersectionObserver((entries)=>{
-//     entries.forEach(entrie=>{
-//         if(entrie.intersectionRatio>0)console.log(entrie)
-//     })
-//   }, options);
 
-//   const obserbables = document.querySelectorAll<HTMLElement>('[on-scroll]')
-
-//   obserbables.forEach(observable=>{
-//     observer.observe(observable) =new IntersectionObserver((entries)=>{
-//     entries.forEach(entrie=>{
-//         if(entrie.intersectionRatio>0)console.log(entrie)
-//     })
-//   }, options);
-//   })
-
-export class ObserbleWrapper extends HTMLElement{
-    static _options = {
-        rootMargin: '0px',
-        threshold: 0.4
-      }
-    static _observer= new IntersectionObserver((entries)=>{
-        entries.forEach(entrie=>{
-            if(entrie.intersectionRatio>0)entrie.target?.classList.remove('not-visible')
-        })
-      }, options);
-
-      private _observables?:NodeListOf<HTMLElement>
-
-    connectedCallback(){
-        if ('IntersectionObserver' in window) {
-            this._observables = this.querySelectorAll<HTMLElement>('[anim-onscroll]')
-            this._observables.forEach(observable=>{
-                ObserbleWrapper._observer.observe(observable)
-                observable.classList.add('not-visible')
-                
-            })
-        } 
-    }
-
-    disconnectedCallback(){
-        this._observables?.forEach(observable=>{
-            ObserbleWrapper._observer.unobserve(observable)
-            
-        })
-    }
+  disconnectedCallback() {
+    this._observables?.forEach((observable) => {
+      ObserbleWrapper._observer.unobserve(observable);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import "./styles/index.css";
 import "@fontsource/archivo-narrow";
-import {ImageGallery} from "./Image-gallery";
+import { ImageGallery } from "./Image-gallery";
 import { initPageTransition } from "./pageTransitions";
 import { initActuWindow } from "./actuWindow";
 import { ObserbleWrapper } from "./Observable-wrapper";
 
+customElements.define("image-gallery", ImageGallery);
+customElements.define("observable-wrapper", ObserbleWrapper, { extends: "a" });
 
-customElements.define('image-gallery', ImageGallery)
-customElements.define('observable-wrapper', ObserbleWrapper)
-
-initActuWindow()
-initPageTransition()
+initActuWindow();
+initPageTransition();
